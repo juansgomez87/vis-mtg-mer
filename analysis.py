e9aa2f9580135c9ed349cbb7d6d7a6a674fefafc
juansgomez87 <juans.gomez87@gmail.com>
2022-03-14 16:19:41 +0100

    added baancing options

--- analysis.py
+++ analysis.py
@@ -217,7 +217,7 @@ if __name__ == "__main__":
     # path_models_users = './models/users/'
     # path_models_users = './models/users_q5_e10/'
     # path_models_users = './models/users_q4_e15/'
-    path_models_users = './models/users_q9_e7/'
+    path_models_users = './models/users_q4_e12_s1958/'
 
     # load data and format for plotting
     res_list = [os.path.join(root, f) for root, dirs, files in os.walk(path_models_users) for f in files if f.lower().endswith('f1.csv')]
